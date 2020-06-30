20f0e359ed538d5803c9e840f56752c4cb788c3e
kosday <47530763+kosday@users.noreply.github.com>
2020-06-30 15:57:35 +0200

    Update to LinkarClient 2.0.0.7

--- LinkarRESTAPITest.php
+++ LinkarRESTAPITest.php
@@ -12,12 +12,12 @@ function Read ($type){
 
 		if ($type == "JSON")
 		{
-			curl_setopt($curl, CURLOPT_POSTFIELDS, "{ \"APIKey\" : \"my_apikey\", \"Data\" : { \"CALCULATED\" : \"False\", \"CONVERSION\" : \"False\", \"FORMAT_SPEC\" : \"False\", \"ORIGINAL_RECORDS\" : \"False\" ,\"CUSTOM_VARS\" : \"\" ,\"OUTPUT_FORMAT\" : \"MV\" ,\"FILE_NAME\" : \"LK.CUSTOMERS\" ,\"RECORDS\" : [ { \"LKITEMID\" : \"2\" }, { \"LKITEMID\" : \"3\" } ] ,\"DICT_CLAUSE\" : \"\"} }");	
+			curl_setopt($curl, CURLOPT_POSTFIELDS, "{ \"APIKey\" : \"my_apikey\", \"Data\" : { \"CALCULATED\" : \"False\", \"CONVERSION\" : \"False\", \"FORMAT_SPEC\" : \"False\", \"ORIGINAL_RECORDS\" : \"False\" ,\"CUSTOM_VARS\" : \"\" ,\"OUTPUT_FORMAT\" : \"JSON\" ,\"FILE_NAME\" : \"LK.CUSTOMERS\" ,\"RECORDS\" : [ { \"LKITEMID\" : \"2\" }, { \"LKITEMID\" : \"3\" } ] ,\"DICT_CLAUSE\" : \"\"} }");	
 			curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
 		}
 		else
 		{
-			curl_setopt($curl, CURLOPT_POSTFIELDS, "<LkCommand><APIKey>my_apikey</APIKey><Data><CALCULATED>False</CALCULATED><CONVERSION>False</CONVERSION><FORMAT_SPEC>False</FORMAT_SPEC><ORIGINAL_RECORDS>False</ORIGINAL_RECORDS><CUSTOM_VARS></CUSTOM_VARS><OUTPUT_FORMAT>MV</OUTPUT_FORMAT><FILE_NAME>LK.CUSTOMERS</FILE_NAME><RECORDS><RECORD><LKITEMID>2</LKITEMID></RECORD><RECORD><LKITEMID>3</LKITEMID></RECORD></RECORDS><DICT_CLAUSE></DICT_CLAUSE></Data></LkCommand>");	
+			curl_setopt($curl, CURLOPT_POSTFIELDS, "<LkCommand><APIKey>my_apikey</APIKey><Data><CALCULATED>False</CALCULATED><CONVERSION>False</CONVERSION><FORMAT_SPEC>False</FORMAT_SPEC><ORIGINAL_RECORDS>False</ORIGINAL_RECORDS><CUSTOM_VARS></CUSTOM_VARS><OUTPUT_FORMAT>XML</OUTPUT_FORMAT><FILE_NAME>LK.CUSTOMERS</FILE_NAME><RECORDS><RECORD><LKITEMID>2</LKITEMID></RECORD><RECORD><LKITEMID>3</LKITEMID></RECORD></RECORDS><DICT_CLAUSE></DICT_CLAUSE></Data></LkCommand>");	
 			curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: text/xml',));
 		}
 		
